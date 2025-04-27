5fc7ad0811f30aa68911effff26c3e4b3d8c0e17
Miles Hill <Miles.Hill@mail.nidec.com>
2025-04-27 10:37:01 +0100

    Initial messaging port

---
+++ src/Milo.Messaging.WPF/MessagePanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Milo.Messaging.WPF
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class MessagePanel : Control
+    {
+        static MessagePanel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MessagePanel), new FrameworkPropertyMetadata(typeof(MessagePanel)));
+        }
+    }
+}
---
+++ src/Milo.Messaging/MessageService.cs
@@ -0,0 +1,31 @@
+﻿using Milo.Core.Messaging;
+
+namespace Milo.Messaging
+{
+    internal class MessageService : IMiloMessageService
+    {
+        public void Start()
+        {
+           
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        public event EventHandler<MessageEventArgs>? MessageSent;
+
+        public bool Send(IMiloMessage message)
+        {
+            if (MessageSent != null)
+            {
+                MessageSent.Invoke(this, new MessageEventArgs(message));
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
